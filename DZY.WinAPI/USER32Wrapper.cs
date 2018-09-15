af989022e2d0ab3c285ca61d7e7ed9f587b3c55c
DaZiYuan <mscoder@hotmail.com>
2018-09-15 23:18:29 +0800

    GetWindowLong

--- DZY.WinAPI/USER32Wrapper.cs
+++ DZY.WinAPI/USER32Wrapper.cs
@@ -426,15 +426,122 @@ namespace DZY.WinAPI
         public int Bottom;
     }
 
+    public abstract class WindowLongConstants
+    {
+        public const int GWL_WNDPROC = -4;
+        public const int GWL_HINSTANCE = -6;
+        public const int GWL_HWNDPARENT = -8;
+        public const int GWL_ID = -12;
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+        public const int GWL_USERDATA = -21;
+    }
+
+    /// <summary>
+    /// WindowStyles and Extended Window Styles
+    /// </summary>
+    public abstract class WindowStyles
+    {
+        public const uint WS_OVERLAPPED = 0x00000000;
+        public const uint WS_POPUP = 0x80000000;
+        public const uint WS_CHILD = 0x40000000;
+        public const uint WS_MINIMIZE = 0x20000000;
+        public const uint WS_VISIBLE = 0x10000000;
+        public const uint WS_DISABLED = 0x08000000;
+        public const uint WS_CLIPSIBLINGS = 0x04000000;
+        public const uint WS_CLIPCHILDREN = 0x02000000;
+        public const uint WS_MAXIMIZE = 0x01000000;
+        public const uint WS_CAPTION = 0x00C00000;     /* WS_BORDER | WS_DLGFRAME  */
+        public const uint WS_BORDER = 0x00800000;
+        public const uint WS_DLGFRAME = 0x00400000;
+        public const uint WS_VSCROLL = 0x00200000;
+        public const uint WS_HSCROLL = 0x00100000;
+        public const uint WS_SYSMENU = 0x00080000;
+        public const uint WS_THICKFRAME = 0x00040000;
+        public const uint WS_GROUP = 0x00020000;
+        public const uint WS_TABSTOP = 0x00010000;
+
+        public const uint WS_MINIMIZEBOX = 0x00020000;
+        public const uint WS_MAXIMIZEBOX = 0x00010000;
+
+        public const uint WS_TILED = WS_OVERLAPPED;
+        public const uint WS_ICONIC = WS_MINIMIZE;
+        public const uint WS_SIZEBOX = WS_THICKFRAME;
+        public const uint WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
+
+        // Common Window Styles
+
+        public const uint WS_OVERLAPPEDWINDOW =
+            (WS_OVERLAPPED |
+              WS_CAPTION |
+              WS_SYSMENU |
+              WS_THICKFRAME |
+              WS_MINIMIZEBOX |
+              WS_MAXIMIZEBOX);
+
+        public const uint WS_POPUPWINDOW =
+            (WS_POPUP |
+              WS_BORDER |
+              WS_SYSMENU);
+
+        public const uint WS_CHILDWINDOW = WS_CHILD;
+
+        //Extended Window Styles
+
+        public const uint WS_EX_DLGMODALFRAME = 0x00000001;
+        public const uint WS_EX_NOPARENTNOTIFY = 0x00000004;
+        public const uint WS_EX_TOPMOST = 0x00000008;
+        public const uint WS_EX_ACCEPTFILES = 0x00000010;
+        public const uint WS_EX_TRANSPARENT = 0x00000020;
+
+
+        public const uint WS_EX_MDICHILD = 0x00000040;
+        public const uint WS_EX_TOOLWINDOW = 0x00000080;
+        public const uint WS_EX_WINDOWEDGE = 0x00000100;
+        public const uint WS_EX_CLIENTEDGE = 0x00000200;
+        public const uint WS_EX_CONTEXTHELP = 0x00000400;
+
+
+        public const uint WS_EX_RIGHT = 0x00001000;
+        public const uint WS_EX_LEFT = 0x00000000;
+        public const uint WS_EX_RTLREADING = 0x00002000;
+        public const uint WS_EX_LTRREADING = 0x00000000;
+        public const uint WS_EX_LEFTSCROLLBAR = 0x00004000;
+        public const uint WS_EX_RIGHTSCROLLBAR = 0x00000000;
+
+
+        public const uint WS_EX_CONTROLPARENT = 0x00010000;
+        public const uint WS_EX_STATICEDGE = 0x00020000;
+        public const uint WS_EX_APPWINDOW = 0x00040000;
+
+
+        public const uint WS_EX_OVERLAPPEDWINDOW = (WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE);
+        public const uint WS_EX_PALETTEWINDOW = (WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST);
+
+
+        public const uint WS_EX_LAYERED = 0x00080000;
+
+
+        public const uint WS_EX_NOINHERITLAYOUT = 0x00100000; // Disable inheritence of mirroring by children
+        public const uint WS_EX_LAYOUTRTL = 0x00400000; // Right to left mirroring
+
+
+        public const uint WS_EX_COMPOSITED = 0x02000000;
+        public const uint WS_EX_NOACTIVATE = 0x08000000;
+    }
+
     public class User32Wrapper
     {
         public const int SPI_SETDESKWALLPAPER = 20;
         public const int SPIF_UPDATEINIFILE = 0x01;
         public const uint SPI_GETDESKWALLPAPER = 0x0073;
 
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hwnd, int index);
+
         [DllImport("user32.dll", EntryPoint = "GetWindowRect")]
         public static extern bool GetWindowRect(IntPtr hWnd, out PRECT lpRect);
-        
+
         public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
 
         [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows", ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
@@ -514,6 +621,21 @@ namespace DZY.WinAPI
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        public static string GetClassName(IntPtr hwnd)
+        {
+            string sResult = string.Empty;
+            StringBuilder ClassName = new StringBuilder(200);
+            //Get the window class name
+            int result = GetClassName(hwnd, ClassName, ClassName.Capacity);
+
+            if (result != 0)
+            {
+                sResult = ClassName.ToString();
+            }
+
+            return sResult;
+        }
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern int GetWindowTextLength(IntPtr hWnd);
