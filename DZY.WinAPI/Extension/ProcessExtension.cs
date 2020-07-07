5629f36446fab7b8841b7384733c4f2c1e42ea28
DaZiYuan <mscoder@hotmail.com>
2020-07-07 11:20:42 +0800

    进程挂起/恢复

---
+++ DZY.WinAPI/Extension/ProcessExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DZY.WinAPI.Extension
+{
+    public static class ProcessExtension
+    {
+        public static void Suspend(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = Kernel32Wrapper.OpenThread(Kernel32Wrapper.ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                Kernel32Wrapper.SuspendThread(pOpenThread);
+            }
+        }
+        public static void Resume(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = Kernel32Wrapper.OpenThread(Kernel32Wrapper.ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                Kernel32Wrapper.ResumeThread(pOpenThread);
+            }
+        }
+    }
+}
